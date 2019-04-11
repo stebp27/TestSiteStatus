9246368b414cbc73f62dd7912764e4d9f4cf703c
Stefano Beltrami <stefanobeltramip27@gmail.com>
2019-04-11 10:26:16 +0200

    Added cronjob to script and added check for status 000

--- SiteStatusScript.sh
+++ SiteStatusScript.sh
@@ -6,45 +6,55 @@
 
 pattern_1="[www.][a-zA-Z0-9]{1,}\.[a-z]{2,4}"
 pattern_2="[a-zA-Z0-9]{1,}\.[a-z]{2,4}"
+pattern_ip="[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
 message=""
 
+echo $1
+echo $2
+
 #verifica se ciò che ha ricevuto come parametro è valido
-if [[ $1 =~ $pattern1 ]] || [[ $1 =~ $pattern2 ]] 
+if [[ $1 =~ $pattern_1 ]] || [[ $1 =~ $pattern_2 ]] || [[ $1 =~ $pattern_ip ]]
 then
-	code=$(printf "GET / HTTP/1.0\r\n\r\n" | nc $1 80 | head -1 | cut -f 2 -d' ')
-	
+	echo "Il parametro è giusto"
+	code=$(curl -s -o /dev/null -w "%{http_code}" $1:$2)
+	echo "Il codice è $code"	
 else
 	echo "Invalid url"
 fi
 
 #verifica della risposta
-if [ $code -ge 100 ] && [ $code -lt 200 ]
+if [[ $code -eq 000 ]]
+then
+	echo "$code"
+	message="Client abort"
+	ruby ~/SiteStatusScript/RubyScripts/EmailRubySender.rb $3 $code
+elif [[ $code -ge 100 ]] && [[ $code -lt 200 ]]
 then
         echo "$code"
 	message="Informational"
-elif [ $code -ge 200 ] && [ $code -lt 300 ]
+elif [[ $code -ge 200 ]] && [[ $code -lt 300 ]]
 then 
 	echo "$code"
 	message="Success"
-	elif [ $code -ge 300 ] && [ $code -lt 400 ]
+	elif [[ $code -ge 300 ]] && [[ $code -lt 400 ]]
 then
 	echo "$code"
 	message="Redirection"
-elif [ $code -ge 400 ] && [ $code -lt 500 ]
+elif [[ $code -ge 400 ]] && [[ $code -lt 500 ]]
 then
 	echo "$code"
 	message="Client Error"
-elif [ $code -ge 500 ] && [ $code -lt 600 ]
+elif [[ $code -ge 500 ]] && [[ $code -lt 600 ]]
 then
 	echo "$code"
 	message="Server Error"
-	ruby ~/SiteStatusScript/RubyScripts/EmailRubySender.rb $2 $code
+	ruby ~/SiteStatusScript/RubyScripts/EmailRubySender.rb $3 $code
 else
 	echo "There's something wrong"
 fi
 
 #opzione -v
-if [[ $3 = "-v" ]]
+if [[ $4 = "-v" ]]
 then
 	echo $message
 	#festival funziona tramite echo, ma non scrive il testo, lo legge solamente
